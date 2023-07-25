0a22076abf0d8e0e1e4493d0651f3604033a54c6
Namitamunjal <70011579+Namitamunjal@users.noreply.github.com>
2023-07-25 11:37:22 +0530

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -9,7 +9,7 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
-const nfts=[];
+const nfts=[]; //empty array to store metadata
 
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
@@ -22,11 +22,13 @@ function mintNFT (a,b,c) {
         "eye color": c,
     }
     nfts.push(metadata);
+    //metadata object is added to the nfts array using the push method.
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {  
+    //The listNFTs function uses a for loop to iterate over the nfts array, printing each metadata object to the console using console.log().
     for(i=0;i<nfts.length;i++)
     {
         console.log(nfts[i]);
@@ -34,11 +36,24 @@ function listNFTs () {
 }
 
 // print the total number of NFTs we have minted to the console
+//
 function getTotalSupply() {
     console.log(nfts.length);
 }
 
+//function call syntax
+//function-name(<parameters>);
+
+
+
 // call your functions below this line
+
+//These lines call the functions to demonstrate how they work. 
+//The mintNFT function is called with three values, and an NFT metadata object is created and added to the nfts array. 
 mintNFT("ABC",18,"black");
+
+//listNFTs is called to print the metadata of all NFTs in the array to the console,
 listNFTs ();
+
+//getTotalSupply is called to print the total number of NFTs in the array
 getTotalSupply();
